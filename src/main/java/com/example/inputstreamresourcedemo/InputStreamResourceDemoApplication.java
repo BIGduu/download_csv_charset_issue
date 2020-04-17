de74b56a2947783044322dc3ddef547a7a9a1584
bigduu <mugeng.du@qq.com>
2020-04-18 02:39:14 +0800

    add BOM header

--- src/main/java/com/example/inputstreamresourcedemo/InputStreamResourceDemoApplication.java
+++ src/main/java/com/example/inputstreamresourcedemo/InputStreamResourceDemoApplication.java
@@ -8,10 +8,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 
 @RestController
 @SpringBootApplication
@@ -22,31 +23,27 @@ public class InputStreamResourceDemoApplication {
     }
 
     @GetMapping(path = "/test")
-    public ResponseEntity<InputStreamResource> test() throws IOException {
-        InputStream resourceAsStream = ClassLoader.getSystemClassLoader().getResourceAsStream("test.txt");
-        byte[] bytes1;
-        bytes1 = new byte[resourceAsStream.available()];
-        resourceAsStream.read(bytes1);
-//        String strjson = new String(bytes1, StandardCharsets.UTF_8);
+    public void test(HttpServletResponse response) throws IOException {
+        char[] chars = new char[1024];
+        InputStream inputStream = Objects.requireNonNull(ClassLoader.getSystemClassLoader().getResourceAsStream("test.txt"));
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+        StringBuilder stringBuilder = new StringBuilder();
+        int read;
+        for (;;){
+            read = inputStreamReader.read(chars);
+            if (read != -1){
+                stringBuilder.append(chars);
+            } else {
+                break;
+            }
+        }
 
-//        byte[] bytes0 = new Byte{new Byte('\uFEFF')}
-//		byte[] bytes1 = "abcdefg中文测试".getBytes();
-        InputStream inputStream = new ByteArrayInputStream(bytes1);
-        HttpHeaders header = new HttpHeaders();
-        header.add("content-disposition", "attachment; filename=test.csv; charset=utf-8");
-//        header.add("Content-Type", "text/csv");
-        header.add("Cache-Control", "no-cache, no-store");
-        header.add("Pragma", "no-cache");
-        header.add("Expires", "0");
-        header.add("Content-Encoding", "UTF-8");
-        header.add("Character-Encoding", "UTF-8");
-        return ResponseEntity.ok()
-//            .contentType(MediaType.valueOf("text/csv;charset=UTF-8"))
-            .headers(
-                header
-            ).body(
-//                strjson
-                new InputStreamResource(inputStream)
-            );
+        response.addHeader("content-disposition", "attachment; filename=test.csv; charset=utf-8");
+        ServletOutputStream outputStream = response.getOutputStream();
+        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream, StandardCharsets.UTF_8);
+        // add BOM info jianshu.com/p/928661e27588
+        outputStreamWriter.write(new String(new byte[] { (byte) 0xEF, (byte) 0xBB,(byte) 0xBF }));
+        outputStreamWriter.write(stringBuilder.toString());
+        outputStreamWriter.flush();
     }
 }
